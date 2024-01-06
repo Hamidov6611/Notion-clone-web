8c38cc878ce88a770871f6dfd03f3bb8f1b21e94
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-01-06 21:41:53 +0500

    Document

--- convex/document.ts
+++ convex/document.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const createDocument = mutation({
@@ -24,5 +24,30 @@ export const createDocument = mutation({
     });
 
     return document;
+  },
+});
+export const getDocuments = query({
+  args: {
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+
+    return documents;
   },
 });
